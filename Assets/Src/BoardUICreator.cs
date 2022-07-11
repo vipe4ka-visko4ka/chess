ac09eafe1e747684a763a70bab90fd9227c4b3c9
Pavlo Makukha <Pavlo.Makukha@ibm.com>
2022-07-11 08:58:04 +0300

    Separate board ui generation logic

---
+++ Assets/Src/BoardUICreator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardUICreator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject squarePrefab;
+
+    private readonly int BOARD_SIZE = 8;
+
+    private void Start()
+    {
+        MakeBoardUI();
+    }
+
+    private void MakeBoardUI()
+    {
+        GameObject squareParent = new GameObject("Squares");
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int coll = 0; coll < BOARD_SIZE; coll++) {
+                GameObject square = Instantiate(squarePrefab, new Vector3(row, 0, coll), Quaternion.identity, squareParent.transform);
+                square.GetComponentInChildren<Renderer>().material.color = row % 2 == coll % 2 ? Color.black : Color.white;
+            }
+        }
+    }
+}
--- Assets/Src/Program.cs
+++ Assets/Src/Program.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class Program : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject squarePrefab;
-    private readonly int BOARD_SIZE = 8;
     private SquareSelector _squareSelector;
     private MoveSelector _moveSelector;
     private List<Piece> _pieces = new List<Piece>();
@@ -19,7 +16,6 @@ public class Program : MonoBehaviour
 
     private void Start()
     {
-        MakeBoardUI();
         MakePieces();
     }
 
@@ -28,16 +24,6 @@ public class Program : MonoBehaviour
         HandleInput();
     }
 
-    private void MakeBoardUI()
-    {
-        GameObject squareParent = new GameObject("Squares");
-        for (int row = 0; row < BOARD_SIZE; row++) {
-            for (int coll = 0; coll < BOARD_SIZE; coll++) {
-                GameObject square = Instantiate(squarePrefab, new Vector3(row, 0, coll), Quaternion.identity, squareParent.transform);
-                square.GetComponentInChildren<Renderer>().material.color = row % 2 == coll % 2 ? Color.black : Color.white;
-            }
-        }
-    }
 
     private void MakePieces()
     {
